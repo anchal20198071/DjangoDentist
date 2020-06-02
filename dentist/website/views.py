1cc4cb030aaa77c730c8aabf53645cc35a1bbee0
Anchal <anchalyadav3420@gmail.com>
2020-06-02 21:06:34 +0530

    contact.html changed accordingly

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html', {})
 def contact(request):
-    return render(request,'contact.html', {})
+    if request.method == 'POST':
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        return render(request, 'contact.html', {'message_name':message_name})
+
+    else:
+        return render(request,'contact.html', {})
